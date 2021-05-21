693db79225497585a5e96958c3ba602c3f6d2ec6
Yaser Haddad Eddahmane <47853373+yaseerhee@users.noreply.github.com>
2021-05-22 01:38:47 +0200

    Crear servidor con puerto donde desplegar app(handler)PRUEBAS EN MAIN

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,1 +1,28 @@
 package handlers
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+// Controladores sirve para crear mi puerto, el controlador y pongo a escuchar al servidor en el 8080
+func Controladores() {
+	router := mux.NewRouter()
+
+	//Miramos si tenemos un puerto
+	PORT := os.Getenv("PORT")
+	// Si no hay un puerto, vamos a forzar a que salga por el puerot 8080
+	if PORT == "" {
+		PORT = "8080"
+	}
+	// Con Cors estamos permitiendo a todo el mundo el acceso desde cualquier lugar
+	handler := cors.AllowAll().Handler(router)
+	log.Fatal(http.ListenAndServe(":"+PORT, handler))
+	// Esto lo que hace es que a la URL que yo ingrese le añade el puerto 8080
+	// A partir de este momento cors pasa a tener el control de la web
+
+}
--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"log"
 
-func main(){
-	
+	"github.com/yaseerhee/YOSKI/bd"
+	"github.com/yaseerhee/YOSKI/handlers"
+)
+
+func main() {
+	// Revisamos que hay conexion con la BD
+	if bd.ObtenerConexion() == 0 {
+		log.Fatal("Sin Conxion a la BD")
+		return
+	}
+
+	handlers.Controladores()
 }
