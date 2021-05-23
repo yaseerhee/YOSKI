fd68728292367c048ce82c32e351986fb2dd0374
Yaser Haddad Eddahmane <47853373+yaseerhee@users.noreply.github.com>
2021-05-23 12:35:26 +0200

    Creamos la func consulta BuscoPerfil

---
+++ bd/BuscoPerfil.go
@@ -0,0 +1,35 @@
+package bd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/yaseerhee/YOSKI/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func BuscoPerfil(ID string) (models.Negocio, error) {
+	contexto, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	// Conexion BD TABLA
+	db := MongoC.Database("yoski")
+	col := db.Collection("negocio")
+
+	var perfil models.Negocio
+	objID, _ := primitive.ObjectIDFromHex(ID) //nOS DEVUELVE UN OBJETO id
+	// Vamos a buscar POR UN PARAMETRO _ID QUE ESTA IGUALADO A NUESTRO OBJID
+	condicion := bson.M{
+		"_id": objID,
+	}
+	// COnsultamos a nuestra collection por el perfil con ese id
+	err := col.FindOne(contexto, condicion).Decode(&perfil)
+	perfil.Password = "" //Nos sirve para limpiar el valor internamente al tener un omitempty deolvera un vacio
+	if err != nil {
+		fmt.Println("Registro no encontrado" + err.Error())
+		return perfil, err
+	}
+	// Todo fue bien nos devuelkve el perfil
+	return perfil, nil
+}
