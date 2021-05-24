e058cfeadab195361ca050b783e259cf9956b1be
Yaser Haddad Eddahmane <47853373+yaseerhee@users.noreply.github.com>
2021-05-24 19:07:01 +0200

    Creo la parte de verPublicaciones

---
+++ bd/veoPublicacionesSeguidores.go
@@ -0,0 +1,53 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/yaseerhee/YOSKI/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// Sirve para visualizar las publicaciones de mis seguiidores
+func VeoPublicacionesSeguidores(ID string, pagina int) ([]models.DevuelvoPublicacionesSeguidores, bool) {
+	// Creamos un contexto. Traemos el contexto en Background de la bd, le agregamos un parametro que es el tiempo del TimeOut (15 seg)
+	contexto, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	// Defer Es una instruccion que la puedes poner donde quieras que se va a ejecutar como ultima instancia
+	defer cancel()
+	// Nos conectamos a la BD
+	db := MongoC.Database("yoski")
+	//cOGEMOS LA COLECCION NEGOCIO
+	col := db.Collection("relacion")
+	// Obtenemos las primeras 20 páginas
+	pasoPagina := (pagina - 1) * 20
+	// Creamos las consultas
+	consulta := make([]bson.M, 0)
+	// Voy a usar un FrameWork de MongoDB aggregate.
+	//El comando match se encarga de buscar la relacion del id con mi negocio
+	consulta = append(consulta, bson.M{"$match": bson.M{"negocioId": ID}})
+	// El comando lookup nos ayuda a conectar las colecciones
+	consulta = append(consulta, bson.M{
+		"$lookup": bson.M{
+			"from":         "publicacion",
+			"localField":   "negociorelacionid",
+			"foreignField": "negocioId",
+			"as":           "publica",
+		},
+	})
+	// sirve para procesar bien los resultados (logica)
+	consulta = append(consulta, bson.M{"$unwind": "$publicacion"})
+	// sirve para ordenar los datos de forma descendente
+	consulta = append(consulta, bson.M{"$sort": bson.M{"publicacion.fecha": -1}})
+	// para que los recoja de 20 en 20
+	consulta = append(consulta, bson.M{"$skip": pasoPagina})
+	consulta = append(consulta, bson.M{"$limit": 20})
+	// Esta funcion es de un framework de Mongo
+	tabla, err := col.Aggregate(contexto, consulta)
+	// Se crea la tabla, creamos un slice con nuestro modelo
+	var resultado []models.DevuelvoPublicacionesSeguidores
+	err = tabla.All(contexto, &resultado) // Decodifica todo en formato tabla
+	if err != nil {
+		return resultado, false
+	}
+	return resultado, true
+}
---
+++ models/devuelvoPublicacionesSeguidores.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+
+// eS EL MODELO DE DATOS QUE RECIBIMOS DE UN APUBLICACION
+type DevuelvoPublicacionesSeguidores struct {
+	ID                primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
+	NegocioID         string             `bson:"negocioId" json:"negocioId,omitempty"`
+	NegocioRelacionID string             `bson:"negociorelacionId" json:"negocioRelationId,omitempty"`
+	Publicacion       struct {
+		Mensaje string    `bson:"mensaje" json:"mensaje,omitempty"`
+		Fecha   time.Time `bson:"fecha" json:"fecha,omitempty"`
+		ID      string    `bson:"_id" json:"_id,omitempty"`
+	}
+}
