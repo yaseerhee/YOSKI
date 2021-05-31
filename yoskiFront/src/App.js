6436ca327c1ebbb153ec5647406925feb6a21f4a
Yaser Haddad Eddahmane <47853373+yaseerhee@users.noreply.github.com>
2021-05-31 20:46:25 +0200

    Creo apartado de regsitro y login

--- yoskiFront/src/App.js
+++ yoskiFront/src/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import AccesoUsuario from "./componentes/AccesoUsuario/AccesoUsuario";
 
 function App() {
-  const [negocio, setNegocio] = useState(null);
+  const [negocio, setNegocio] = useState({ nombre: "Yaser" });
   return (
     <div>
-      {negocio ? <h1>Sesion iniciada</h1> : <h1>Sesion no iniciada</h1>}
+      {negocio ? (
+        <div>
+          <AccesoUsuario />
+        </div>
+      ) : (
+        <h1>Sesion no iniciada</h1>
+      )}
     </div>
   );
 }
