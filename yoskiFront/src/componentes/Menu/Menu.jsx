701a696efb020c36721efff1d8f5ae8ed1a67f3f
Yaser Haddad Eddahmane <47853373+yaseerhee@users.noreply.github.com>
2021-06-03 17:56:36 +0200

    Añado cerrar sesion (Investigar otra forma de eliminar la sesion

--- yoskiFront/src/componentes/Menu/Menu.jsx
+++ yoskiFront/src/componentes/Menu/Menu.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import {cerrarSesionApi} from "../../api/autentificacion";
+// iconos
 import Logo from '../../img/logo_yoski_verde_claro.png';
 import Inicio from '../../img/inicio.svg'; 
 import Negocio from '../../img/tienda.svg'; 
 import Perfil from '../../img/perfil.svg'; 
 import Salir from '../../img/exit.svg'; 
+
 // Estilos
 import "./Menu.scss";
 
 export default function Menu() {
+
+    const cerrarSesion = () => {
+        //Nos borra la sesio (tokem)
+        cerrarSesionApi();
+        // refrescamos la pagina para qu enos obligue a salir :D es la unica solucion que he encontrado jeje
+        window.location.reload();
+    }
+
     return (
         <div className="menu">
             <img className="logo" src={Logo} alt="yoski" />
             <Link to="/">
-                <img className="icons" src={Inicio} /> Inicio
+                <img className="icons" src={Inicio} alt="inicio"/> Inicio
             </Link>
             <Link to="/perfil">
-                <img className="icons" src={Perfil} /> Perfil
+                <img className="icons" src={Perfil} alt="perfil"/> Perfil
             </Link>
             <Link to="/negocio">
-                <img className="icons" src={Negocio} /> Negocios
+                <img className="icons" src={Negocio} alt="negocios"/> Negocios
             </Link>
-            <Link to="/salir">
-                <img className="icons" src={Salir} /> Salir
+            <Link to="" onClick={cerrarSesion}>
+                <img className="icons" src={Salir} alt="salir"/> Salir
             </Link>
 
             <Button>Publicar</Button>
