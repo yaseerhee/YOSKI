1df1b8f339255cd47c43ef06ce2c6e808e63110f
Yaser Haddad Eddahmane <47853373+yaseerhee@users.noreply.github.com>
2021-06-05 21:11:46 +0200

    Añadimos pagina de error 404 para controlar a quien intente manipular la url

--- yoskiFront/src/pagina/Error404/Error404.jsx
+++ yoskiFront/src/pagina/Error404/Error404.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import Error404IMG from "../../img/error404.jpg";
+import LOGO from "../../img/logo_yoski_verde_claro.png";
 // estilos
 import "./Error404.scss";
 
 export default function Error404() {
     return (
-        <div>         
-            <h2>Error 404</h2>
+        <div className="error404">         
+        <img src={LOGO} alt="logo"/>
+            <h2>Esta web es igual de robusta que mi brazo.</h2>
+            <h2>Osea, mucho</h2>
+            <img src={Error404IMG} alt="error"/>
+            
+            <Link to="/">Pincha aquí para volver</Link>
         </div>
     )
 }
